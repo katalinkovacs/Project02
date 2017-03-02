eab03be98be5e15fcb9e2c69f4a34f734f35bd88
katalinkovacs <katalin.kovacs@marlo.com.au>
2017-03-02 13:54:19 +1100

    PersonInfo2 added --> has been acting weird.......

--- PersonInfo2/src/main/java/personinfo2/Personinfo2.java
+++ PersonInfo2/src/main/java/personinfo2/Personinfo2.java
@@ -2,23 +2,4 @@ package personinfo2;
 
 
 public class Personinfo2 {
-
-        private String givenname;
-        private String familyname;
-
-    public String getGivenname() {
-        return givenname;
-    }
-
-    public void setGivenname(String givenname) {
-        this.givenname = givenname;
-    }
-
-    public String getFamilyname() {
-        return familyname;
-    }
-
-    public void setFamilyname(String familyname) {
-        this.familyname = familyname;
-    }
 }
