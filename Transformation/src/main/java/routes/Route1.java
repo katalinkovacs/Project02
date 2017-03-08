db99e662bbfb5954b2738aa09f6b2b485a4a9fce
katalinkovacs <katalin.kovacs@marlo.com.au>
2017-03-09 08:46:52 +1100

    Rout1Test

--- Transformation/src/main/java/routes/Route1.java
+++ Transformation/src/main/java/routes/Route1.java
@@ -3,7 +3,7 @@ package routes;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.apache.camel.converter.jaxb.JaxbDataFormat;
-import org.apache.camel.model.dataformat.XmlJsonDataFormat;
+//import org.apache.camel.model.dataformat.XmlJsonDataFormat;
 import processors.Processor1;
 
 
@@ -13,16 +13,16 @@ public class Route1 extends RouteBuilder {
 
     public void configure() throws Exception {
 
-        XmlJsonDataFormat xmlJsonFormat = new XmlJsonDataFormat();
+        //XmlJsonDataFormat xmlJsonFormat = new XmlJsonDataFormat();
 
         JacksonDataFormat myEmployee2JsonFormat = new JacksonDataFormat(employee2.Employee2.class);
 
         //marshall method needs a java object called jaxbDataFormat2
         JaxbDataFormat jaxbDataFormat2 = new JaxbDataFormat("employee1");
 
-
         //from("file:C:/Kati/Marlo/GitRepo/FileReads/Project01/From")
-        from("file:Transformation/src/main/resources/data/inbox1?noop=true")
+        from("file:Transformation/src/main/resources/data/inbox/inbox1?noop=true")
+                .id("route1")
                 .unmarshal(myEmployee2JsonFormat)
                 .bean(processor1Object, "processor1Method")   // object + method
                 .marshal(jaxbDataFormat2)
