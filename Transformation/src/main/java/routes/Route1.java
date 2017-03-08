0bd4ca7022819d0546be0e2267924f5fc2888fd5
zolinz <zoltankovacs_nz@yahoo.co.nz>
2017-03-08 15:00:32 +1100

    fixed route

--- Transformation/src/main/java/routes/Route1.java
+++ Transformation/src/main/java/routes/Route1.java
@@ -1,7 +1,8 @@
 package routes;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.converter.jaxb.JaxbDataFormat;//
+import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.apache.camel.converter.jaxb.JaxbDataFormat;
 import org.apache.camel.model.dataformat.XmlJsonDataFormat;
 import processors.Processor1;
 
@@ -14,14 +15,15 @@ public class Route1 extends RouteBuilder {
 
         XmlJsonDataFormat xmlJsonFormat = new XmlJsonDataFormat();
 
+        JacksonDataFormat myEmployee2JsonFormat = new JacksonDataFormat(employee2.Employee2.class);
 
         //marshall method needs a java object called jaxbDataFormat2
-        JaxbDataFormat jaxbDataFormat2 = new JaxbDataFormat("employeeversion2");
+        JaxbDataFormat jaxbDataFormat2 = new JaxbDataFormat("employee1");
 
 
         //from("file:C:/Kati/Marlo/GitRepo/FileReads/Project01/From")
         from("file:Transformation/src/main/resources/data/inbox1?noop=true")
-                .unmarshal(xmlJsonFormat)
+                .unmarshal(myEmployee2JsonFormat)
                 .bean(processor1Object, "processor1Method")   // object + method
                 .marshal(jaxbDataFormat2)
                 .to("file:Transformation/src/main/resources/data/outbox");
